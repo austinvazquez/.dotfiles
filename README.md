1c3d5602e18587be8b3bee6f11a4bdb48f526939
Austin Vazquez <macedonv@amazon.com>
2022-03-21 19:03:35 -0700

    Add tmux configuration
    Signed-off-by: Austin Vazquez <macedonv@amazon.com>

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # .dotfiles
 Linux Bash Configuration
+
+## Installation
+
+`./setup install`
+
+## [Tmux](https://github.com/tmux/tmux/wiki)
+
+Installs `$HOME/.tmux.conf` and creates a backup of existing configuration ($HOME/.tmux.conf.back).
+
+Rebind prefix to `Ctrl+b`.
+
+`hjkl` for navigation.
+
---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+command=$1
+
+cd ~
+
+if [[ "$command" == "install" ]]; then
+    # Install tmux config.
+    if [[ -f .tmux.conf ]]; then
+        cp .tmux.conf .tmux.conf.back
+    fi
+    cp .dotfiles/.tmux.conf .
+elif [[ "$command" == "uninstall" ]]; then
+    # Check if we have a backup of previous config.
+    if [[ -f .tmux.conf.back ]]; then
+        mv .tmux.conf.back .tmux.conf
+    else
+        rm .tmux.conf
+    fi
+fi
+
